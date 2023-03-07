1f8f47f7e9ee94d0b11f330851a91a10abdbdcdd
Daniloel <zoltancao@gmail.com>
2023-03-07 20:46:59 -0300

    Arquivos do Projeto

---
+++ src/main/java/LAV/deprojetospring20231/DeprojetoSpring20231Application.java
@@ -0,0 +1,13 @@
+package LAV.deprojetospring20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeprojetoSpring20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeprojetoSpring20231Application.class, args);
+	}
+
+}
---
+++ src/test/java/LAV/deprojetospring20231/DeprojetoSpring20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package LAV.deprojetospring20231;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeprojetoSpring20231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
